cd69c4a2af1c165df97095aa7c865b6ec8db4d8c
MohammadHassanRao <hassanribery0335@gmail.com>
2021-03-22 02:22:58 +0500

    deployed to heroku

--- server.js
+++ server.js
@@ -47,13 +47,13 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin: https://food-mania.herokuapp.com");
-//   res.header("Access-Control-Allow-Credentials: true");
-//   res.header("Access-Control-Allow-Methods: GET, POST");
-//   res.header("Access-Control-Allow-Headers: Content-Type, *");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin: https://food-mania.herokuapp.com");
+  res.header("Access-Control-Allow-Credentials: true");
+  res.header("Access-Control-Allow-Methods: GET, POST");
+  res.header("Access-Control-Allow-Headers: Content-Type, *");
+  next();
+});
 
 app.use("/", express.static(path.resolve(path.join(__dirname, "./web/build"))));
 
