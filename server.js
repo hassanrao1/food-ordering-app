9a7dfb1c3de412851386c5ee497ddfc046778562
MohammadHassanRao <hassanribery0335@gmail.com>
2021-03-22 02:26:46 +0500

    deployed to heroku

--- server.js
+++ server.js
@@ -42,18 +42,18 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://food-mania.herokuapp.com",
+    origin: ["http://localhost:3000", "https://food-mania.herokuapp.com"],
     credentials: true,
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin: https://food-mania.herokuapp.com");
-  res.header("Access-Control-Allow-Credentials: true");
-  res.header("Access-Control-Allow-Methods: GET, POST");
-  res.header("Access-Control-Allow-Headers: Content-Type, *");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin: https://food-mania.herokuapp.com");
+//   res.header("Access-Control-Allow-Credentials: true");
+//   res.header("Access-Control-Allow-Methods: GET, POST");
+//   res.header("Access-Control-Allow-Headers: Content-Type, *");
+//   next();
+// });
 
 app.use("/", express.static(path.resolve(path.join(__dirname, "./web/build"))));
 
