3a4fb1a4b8ad7ac186f8277f55323096a05e7bd4
MohmmadHassanRao <hassanribery0335@gmail.com>
2021-04-13 14:00:56 -0700

    search bar in admin

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get(
       httpOnly: true,
     });
 
-    res.redirect("http://localhost:3000/#/dashboard");
+    res.redirect("https://food-mania.herokuapp.com/#/dashboard");
     // console.log("131", req.cookie);
 
     // Successful authentication, redirect home.
